2ee2a13e11f1b4d7d1c8bf7e4670d62c96336a7e
IngPipex06 <fellippex06@hotmail.com>
2024-12-05 20:02:46 -0500

    perfect

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3030;
  
 //Configurar las cosas jaja
 const publicPath = path.resolve(__dirname, "../public");
